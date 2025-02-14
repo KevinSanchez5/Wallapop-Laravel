b69f45ddc286fad2feb6bcc537636fd5990065f5
Jaimelen10 <jleonmulero@gmail.com>
2025-02-14 18:21:14 +0100

    creada tabla valoraciones

--- waravel/database/migrations/2025_02_14_165313_create_valoraciones_table.php
+++ waravel/database/migrations/2025_02_14_165313_create_valoraciones_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('valoracions', function (Blueprint $table) {
+        Schema::create('valoraciones', function (Blueprint $table) {
             $table->id();
+            $table->string('guid');
+            $table->string('comentario');
+            $table->integer('puntuacion');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('valoracions');
+        Schema::dropIfExists('valoraciones');
     }
 };
--- waravel/database/migrations/2025_02_14_170022_create_direcciones_table.php
+++ waravel/database/migrations/2025_02_14_170022_create_direcciones_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('direccions', function (Blueprint $table) {
+        Schema::create('direcciones', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('direccions');
+        Schema::dropIfExists('direcciones');
     }
 };
