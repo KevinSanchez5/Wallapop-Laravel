146c7a7cd364fa4eb8d94ac7e93ceb86a9ad572d
Jaimelen10 <jleonmulero@gmail.com>
2025-02-15 16:13:18 +0100

    creada tabla valoraciones y modelo valoraciones con sus relaciones

--- waravel/app/Models/Valoracion.php
+++ waravel/app/Models/Valoracion.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Valoracion extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['guid', 'comentario', 'puntuacion', 'clienteValorado_id', 'autor_id'];
+
+    /**
+     * Relación con el cliente valorado (cliente que recibe la valoración).
+     */
+    public function clienteValorado()
+    {
+        return $this->belongsTo(Cliente::class, 'clienteValorado_id');
+    }
+
+    /**
+     * Relación con el creador de la valoración (cliente que crea la valoración).
+     */
+    public function creador()
+    {
+        return $this->belongsTo(Cliente::class, 'autor_id');
+    }
 }
