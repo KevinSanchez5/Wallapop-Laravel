f8dba96daa23eb3901c08fffadfb515da687df6b
Jaimelen10 <jleonmulero@gmail.com>
2025-02-15 16:12:22 +0100

    creada tabla clientes con modelo cliente y relaciones correspondientes

--- waravel/app/Models/Cliente.php
+++ waravel/app/Models/Cliente.php
@@ -9,9 +9,39 @@ class Cliente extends Model
 {
     use HasFactory;
 
-    // protected $fillable = ['nombre', 'email', 'direccion'];
+    protected $fillable = ['guid', 'nombre', 'apellido', 'avatar', 'telefono', 'direccion', 'activo', 'usuario_id', 'favoritos'];
 
     protected $casts = [
         'direccion' => Direccion::class
     ];
+
+    // Relación 1-1 inversa con User
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'usuario_id');
+    }
+
+    // Relación 1:N con Producto
+    public function productos()
+    {
+        return $this->hasMany(Producto::class, 'vendedor_id');
+    }
+
+    // Relación N:M -> Un cliente puede tener varios productos en favoritos
+    public function favoritos()
+    {
+        return $this->belongsToMany(Producto::class, 'cliente_favoritos', 'cliente_id', 'producto_id')->withTimestamps();
+    }
+
+    // Relación con las valoraciones que ha recibido un cliente.
+    public function valoracionesRecibidas()
+    {
+        return $this->hasMany(Valoracion::class, 'clienteValorado_id');
+    }
+
+    // Relación con las valoraciones que ha creado un cliente.
+    public function valoracionesCreadas()
+    {
+        return $this->hasMany(Valoracion::class, 'creador_id');
+    }
 }
