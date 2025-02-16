74cacebc5445078648dbc7d574cbf1ba07acd020
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-02-17 00:37:33 +0100

    agregado el carrusel component

---
+++ waravel/resources/views/components/carousel.blade.php
@@ -0,0 +1,31 @@
+<div class="w-full relative">
+    <div class="swiper relative">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide relative">
+                <img src="{{ asset('imgs/carusel_1.jpg') }}" alt="Transacciones económicas" class="w-full h-[400px] object-cover">
+                <h2 class="absolute bottom-5 left-5 text-white text-3xl font-bold px-4 py-2 rounded-lg animate__animated animate__fadeIn animate__delay-1.5s">
+                    Transacciones económicas seguras
+                </h2>
+            </div>
+
+            <div class="swiper-slide relative">
+                <img src="{{ asset('imgs/carusel_2.jpg') }}" alt="Si no lo usas" class="w-full h-[400px] object-cover">
+                <h2 class="absolute bottom-5 left-5 text-white text-3xl font-bold px-4 py-2 rounded-lg animate__animated animate__fadeIn animate__delay-1.5s">
+                    Si no lo usas, sácale partido
+                </h2>
+            </div>
+
+            <div class="swiper-slide relative">
+                <img src="{{ asset('imgs/carusel_3.jpg') }}" alt="Ahorra o nunca" class="w-full h-[400px] object-cover">
+                <h2 class="absolute bottom-5 left-5 text-white text-3xl font-bold px-4 py-2 rounded-lg animate__animated animate__fadeIn animate__delay-1.5s">
+                    Ahorra o nunca
+                </h2>
+            </div>
+        </div>
+
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+
+        <div class="swiper-pagination"></div>
+    </div>
+</div>
