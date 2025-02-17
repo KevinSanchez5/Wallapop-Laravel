ed984f291931549c5a1200fda338fd26bf169c0d
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-17 11:15:20 +0100

    hechos seeder de users
    hechos seeder de users

---
+++ waravel/database/seeders/UsersSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UsersSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => 'adminPassword123',
+            'role' => 'admin',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        User::create([
+            'name' => 'Juan Pérez',
+            'email' => 'juan@example.com',
+            'password' => 'Password123',
+            'role' => 'cliente',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        User::create([
+            'name' => 'María García',
+            'email' =>'maria@example.com',
+            'password' => 'Password123',
+            'role' => 'cliente',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        User::create([
+            'name' => 'Pedro Martínez',
+            'email' => 'pedro@example.com',
+            'password' => 'Password123',
+            'role' => 'cliente',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        User::create([
+            'name' => 'Isabella Rodríguez',
+            'email' => 'isabella@example.com',
+            'password' => 'Password123',
+            'role' => 'user',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
