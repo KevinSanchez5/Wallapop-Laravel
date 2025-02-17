20b7c08900159c12f848da60684ff74da4905af2
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-17 20:17:32 +0100

    model Carrito
    model Carrito

--- waravel/app/Models/Carrito.php
+++ waravel/app/Models/Carrito.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Carrito extends Model
 {
-    //
+    protected $fillable = ['guid', 'cliente', 'lineasCarrito', 'precioTotal'];
+
+    public function setClienteAttribute($value)
+    {
+        $this->attributes['cliente'] = json_encode($value);
+    }
+
+    public function getClienteAttribute($value)
+    {
+        return json_decode($value);
+    }
+
+    public function setLineaCarritoAttribute($value)
+    {
+        $this->attributes['lineasCarrito'] = json_encode($value);
+    }
+
+    public function getLineaCarritoAttribute($value)
+    {
+        return json_decode($value);
+    }
 }
