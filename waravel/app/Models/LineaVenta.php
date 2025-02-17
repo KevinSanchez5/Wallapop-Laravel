bac61479fd8499bec1bc01a55104f15bf0dd8329
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-17 20:16:59 +0100

    model LineaVenta
    model LineaVenta

--- waravel/app/Models/LineaVenta.php
+++ waravel/app/Models/LineaVenta.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class LineaVenta extends Model
 {
-    //
+    protected $fillable = ['guid', 'vendedor', 'cantidad', 'producto', 'precio', 'precioTotal'];
+
+    public function setVendedorAttribute($value)
+    {
+        $this->attributes['vendedor'] = json_encode($value);
+    }
+
+    public function getVendedorAttribute($value)
+    {
+        return json_decode($value);
+    }
+
+    public function setProductoAttribute($value)
+    {
+        $this->attributes['producto'] = json_encode($value);
+    }
+
+    public function getProductoAttribute($value)
+    {
+        return json_decode($value);
+    }
 }
