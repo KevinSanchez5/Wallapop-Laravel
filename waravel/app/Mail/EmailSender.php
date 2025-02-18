5fec35b4ce3adb33acbc5bdd9f10953579daea54
KevinSanchez5 <@KevinSanchez5>
2025-02-18 22:37:06 +0100

    Minor fix EmailSender

--- waravel/app/Mail/EmailSender.php
+++ waravel/app/Mail/EmailSender.php
@@ -2,13 +2,13 @@
 
 namespace App\Mail;
 
-use http\Exception\InvalidArgumentException;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use InvalidArgumentException;
 
 class EmailSender extends Mailable
 {
