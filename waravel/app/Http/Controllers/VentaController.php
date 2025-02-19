4d2c0fb7493abcc3901b0813ce7d1abff7505399
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-19 17:19:46 +0100

    arreglados get de ventaController
    arreglados get de ventaController

--- waravel/app/Http/Controllers/VentaController.php
+++ waravel/app/Http/Controllers/VentaController.php
@@ -2,31 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Venta;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Validator;
 
 class VentaController extends Controller
 {
     public function index(){
-        $venta = Venta::all();
-        return response()->json($venta);
+        $query = Venta::orderBy('id', 'asc');
+
+        $ventas = $query->paginate(5);
+
+        $data = $ventas->getCollection()->transform(function ($venta) {
+            return [
+                'id' => $venta->id,
+                'guid' => $venta->guid,
+                'comprador' => $venta->comprador,
+                'lineaVentas' => $venta->lineaVentas,
+                'precioTotal' => $venta->precioTotal,
+                'created_at' => $venta->created_at->toDateTimeString(),
+                'updated_at' => $venta->updated_at->toDateTimeString(),
+            ];
+        });
+
+        $customResponse = [
+            'ventas' => $data,
+            'paginacion' => [
+                'pagina_actual' => $ventas->currentPage(),
+                'elementos_por_pagina' => $ventas->perPage(),
+                'ultima_pagina' => $ventas->lastPage(),
+                'elementos_totales' => $ventas->total(),
+            ],
+        ];
+
+        return response()->json($customResponse);
     }
 
     public function show($id)
     {
         $ventaRedis = Redis::get('venta_'.$id);
-        if($ventaRedis) {
-            return response()->json(json_decode($ventaRedis));
 
+        if ($ventaRedis) {
+            return response()->json(json_decode($ventaRedis, true));
         }
 
         $venta = Venta::find($id);
 
-        if(!$ventaRedis) {
+        if (!$venta) {
             return response()->json(['message' => 'Venta no encontrada'], 404);
         }
 
-        return response()->json($venta);
+        $data = [
+            'id' => $venta->id,
+            'guid' => $venta->guid,
+            'comprador' => $venta->comprador,
+            'lineaVentas' => $venta->lineaVentas,
+            'precioTotal' => $venta->precioTotal,
+            'created_at' => $venta->created_at->toDateTimeString(),
+            'updated_at' => $venta->updated_at->toDateTimeString(),
+        ];
+
+        Redis::set('venta_'. $id, json_encode($data), 'EX',1800);
+
+        return response()->json($data);
     }
 
     public function store(Request $request)
@@ -36,42 +75,15 @@ class VentaController extends Controller
             'comprador' => 'required|array',
             'lineaVentas' => 'required|array',
             'precioTotal' => 'required|numeric|min:0'
-
         ]);
+
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
+
         $venta = Venta::create($request->all());
+
         return response()->json($venta, 201);
-    }
-
-    public function update(Request $request, $id)
-    {
-        $venta = Redis::get('venta_'. $id);
-
-        if(!$venta) {
-            $venta = Venta::find($id);
-        }
-
-        if(!$venta) {
-            return response()->json(['message' => 'Venta no encontrada'], 404);
-        }
-        $validator = Validator::make($request->all(), [
-            'guid' => 'required|string|max:255|unique:ventas',
-            'comprador' => 'required|array',
-            'lineaVentas' => 'required|array',
-            'precioTotal' => 'required|numeric|min:0'
-
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-        $venta->update($request->all());
-        Redis::del('venta_'. $id);
-        Redis::set('venta_'. $id, json_encode($venta), 'EX',1800);
-
-        return response()->json($venta);
     }
 
     public function destroy($id)
