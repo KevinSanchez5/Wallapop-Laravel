4f3ed4b76b3c590657b18a2117914b2fae86bf0f
Alba García Orduña <albag448@gmail.com>
2025-02-19 17:13:08 +0100

    Añadido test de show del cliente controller

--- waravel/tests/Feature/ClienteControllerTest.php
+++ waravel/tests/Feature/ClienteControllerTest.php
@@ -3,8 +3,11 @@
 namespace Tests\Feature;
 
 use App\Models\Cliente;
+use App\Models\Producto;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 
@@ -64,4 +67,58 @@ class ClienteControllerTest extends TestCase
         $this->assertEquals(5, $responseData['paginacion']['elementos_por_pagina']);
         $this->assertEquals(10, $responseData['paginacion']['elementos_totales']);
     }
+
+    public function test_show(): void
+    {
+        $usuario = User::create([
+            'name' => 'Cliente',
+            'email' => 'cliente@example.com',
+            'password' => bcrypt('secret'),
+            'role' => 'cliente',
+        ]);
+
+        $cliente = Cliente::create([
+            'guid' => Str::uuid(),
+            'nombre' => 'Pepe',
+            'apellido' => 'Perez',
+            'avatar' => 'avatar.png',
+            'telefono' => '1234567890',
+            'direccion' => json_encode([
+                'calle' => 'Avenida Siempre Viva',
+                'numero' => 742,
+                'piso' => 1,
+                'letra' => 'A',
+                'codigoPostal' => 28001
+            ]),
+            'activo' => true,
+            'usuario_id' => $usuario->id,
+        ]);
+
+        Redis::set('cliente_' . $cliente->id, json_encode($cliente));
+
+        $response = $this->getJson("/api/clientes/{$cliente->id}");
+        $response->assertStatus(200);
+        $responseData = $response->json();
+        $direccionDecodificada = json_decode($responseData['direccion'], true);
+        
+        $response->assertJson([
+            'id' => $cliente->id,
+            'nombre' => $cliente->nombre,
+            'apellido' => $cliente->apellido,
+            'avatar' => $cliente->avatar,
+            'telefono' => $cliente->telefono,
+            'direccion' => $direccionDecodificada,
+            'activo' => true,
+            'usuario_id' => $usuario->id,
+        ]);
+
+
+        $clienteRedis = json_decode(Redis::get('cliente_' . $cliente->id), true);
+        $this->assertEquals($cliente->id, $clienteRedis['id']);
+        $this->assertEquals($cliente->nombre, $clienteRedis['nombre']);
+        $this->assertEquals($cliente->apellido, $clienteRedis['apellido']);
+    }
+
+
+
 }
