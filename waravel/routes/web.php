bd4094f6ac151a7d9d1a6146c3869382f1a24af6
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-02-20 11:05:18 +0100

    ajustando las rutas

--- waravel/routes/web.php
+++ waravel/routes/web.php
@@ -6,18 +6,13 @@ use App\Http\Controllers\Views\ProductoControllerView;
 use App\Http\Controllers\Views\ValoracionesControllerView;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ProductoControllerView::class, 'indexVista'])->name('inicio');
+Route::get('/', [ProductoControllerView::class, 'indexVista'])->name('pages.home');
 
-Route::get('/productos', [ProductoControllerView::class, 'indexVista'])->name('productos.index');
 Route::get('/producto/{guid}', [ProductoControllerView::class, 'showVista'])->name('producto.show');
 Route::get('/productos/search', [ProductoControllerView::class, 'search'])->name('productos.search');
 Route::get('/cliente/{guid}', [ClienteControllerView::class, 'mostrarCliente'])->name('cliente.ver');
 Route::get('/clientes/{guid}/valoraciones', [ValoracionesControllerView::class, 'index'])->name('cliente.valoraciones');
 Route::get('/clientes/{guid}/puntuacion', [ValoracionesControllerView::class, 'promedio'])->name('cliente.puntuacion');
-
-Route::get('/', function () {
-    return view('inicio');
-})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
