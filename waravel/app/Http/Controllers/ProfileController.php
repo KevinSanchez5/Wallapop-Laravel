974a6586b9d4cfd2bd7a8442ac563ae5a6f420da
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-22 11:47:07 +0100

    arreglado problema con log de perfil
    arreglado problema con log de perfil

--- waravel/app/Http/Controllers/ProfileController.php
+++ waravel/app/Http/Controllers/ProfileController.php
@@ -9,6 +9,7 @@ use App\Models\Valoracion;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
