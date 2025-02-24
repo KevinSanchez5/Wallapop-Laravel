71fb946c58aa3c481bc7be790af616f158949030
Diego NL <diegonoviluce28@gmail.com>
2025-02-24 11:27:44 +0100

    Añado link a continuar explorando

--- waravel/resources/views/pages/shoppingCart.blade.php
+++ waravel/resources/views/pages/shoppingCart.blade.php
@@ -271,7 +271,7 @@
 
                             <div class="flex items-center justify-center gap-2">
                                 <span class="text-sm font-normal text-gray-500 dark:text-gray-400"> o </span>
-                                <a href="" title="" class="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline dark:text-primary-500 dark:text-white">
+                                <a href="{{ route("pages.home") }}" title="" class="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline dark:text-primary-500 dark:text-white">
                                     Continuar explorando
                                     <svg class="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
