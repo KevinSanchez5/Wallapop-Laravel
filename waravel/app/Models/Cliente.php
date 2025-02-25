90318846d1521defbd5f34c33d5351ff4960d3a2
Alba García Orduña <albag448@gmail.com>
2025-02-25 16:23:06 +0100

    Cambiando id por guid en el model cliente

--- waravel/app/Models/Cliente.php
+++ waravel/app/Models/Cliente.php
@@ -9,36 +9,36 @@ class Cliente extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['guid', 'nombre', 'apellido', 'avatar', 'telefono', 'direccion', 'activo', 'usuario_id'];
+    protected $fillable = ['guid', 'nombre', 'apellido', 'avatar', 'telefono', 'direccion', 'activo', 'usuario_guid'];
 
     // Relación 1-1 inversa con User
     public function usuario()
     {
-        return $this->belongsTo(User::class, 'usuario_id');
+        return $this->belongsTo(User::class, 'usuario_guid');
     }
 
     // Relación 1:N con Producto
     public function productos()
     {
-        return $this->hasMany(Producto::class, 'vendedor_id');
+        return $this->hasMany(Producto::class, 'vendedor_guid');
     }
 
     // Relación N:M -> Un cliente puede tener varios productos en favoritos
     public function favoritos()
     {
-        return $this->belongsToMany(Producto::class, 'cliente_favoritos', 'cliente_id', 'producto_id')->withTimestamps();
+        return $this->belongsToMany(Producto::class, 'cliente_favoritos', 'cliente_guid', 'producto_guid')->withTimestamps();
     }
 
     // Relación con las valoraciones que ha recibido un cliente.
     public function valoracionesRecibidas()
     {
-        return $this->hasMany(Valoracion::class, 'clienteValorado_id');
+        return $this->hasMany(Valoracion::class, 'clienteValorado_guid');
     }
 
     // Relación con las valoraciones que ha creado un cliente.
     public function valoracionesCreadas()
     {
-        return $this->hasMany(Valoracion::class, 'creador_id');
+        return $this->hasMany(Valoracion::class, 'creador_guid');
     }
 
     public function setDireccionAttribute($value)
