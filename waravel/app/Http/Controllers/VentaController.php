6b7f78f7a08e4439ba3151d5516a87aaa36d49bb
Alba García Orduña <albag448@gmail.com>
2025-02-25 16:10:54 +0100

    Cambiados id por guid

--- waravel/app/Http/Controllers/VentaController.php
+++ waravel/app/Http/Controllers/VentaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Venta;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Validator;
 
