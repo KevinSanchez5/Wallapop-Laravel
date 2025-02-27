dd4d5387812a06e9d8b3e26c71088ad4761ad132
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-02-27 17:42:10 +0100

    areglando user seeders

--- waravel/database/seeders/UsersSeeder.php
+++ waravel/database/seeders/UsersSeeder.php
@@ -62,7 +62,6 @@ class UsersSeeder extends Seeder
         ]);
 
         User::create([
-            'guid'=> 'a2b3c4d5-1234-5678-9abc-def012345678',
             'name' => 'Ana López',
             'email' => 'ana@example.com',
             'password' => 'AdminPass123!',
@@ -76,7 +75,6 @@ class UsersSeeder extends Seeder
         ]);
 
         User::create([
-            'guid'=> 'f6e7d8c9-9876-5432-b1a2-c3d4e5f6g7h8',
             'name' => 'Carlos Fernández',
             'email' => 'carlos@example.com',
             'password' => 'SuperAdmin456!',
@@ -90,7 +88,6 @@ class UsersSeeder extends Seeder
         ]);
 
         User::create([
-            'guid'=> '9d00acfc-64b4-4406-9de9-5988aa3e4816',
             'name' => 'Isabella Rodríguez',
             'email' => 'isabella@example.com',
             'password' => 'Password123?',
@@ -104,7 +101,6 @@ class UsersSeeder extends Seeder
         ]);
 
         User::create([
-            'guid'=> '4b081c14-d84b-4994-b521-57763b87483a',
             'name' => 'Mario de Domingo',
             'email' => 'wolverine.mda.307@gmail.com',
             'password' => '30072004',
