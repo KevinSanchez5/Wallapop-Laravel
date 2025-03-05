44fc02f990f40d2bc9223418743cc069cdcce0a7
Jaimelen10 <jleonmulero@gmail.com>
2025-03-05 20:43:32 +0100

    Modificada busqueda de favs de cliente autenticado

--- waravel/app/Http/Controllers/Views/ClienteControllerView.php
+++ waravel/app/Http/Controllers/Views/ClienteControllerView.php
@@ -18,7 +18,14 @@ class ClienteControllerView extends Controller
             ->where('estado', 'Disponible')
             ->get();
 
-        return view('pages.ver-cliente', compact('cliente', 'productos'));
+        if (auth()->check()) {
+            $clienteAuth = auth()->user()->cliente;
+            $productosFavoritos = $clienteAuth ? $clienteAuth->favoritos->pluck('id')->toArray() : [];
+        } else {
+            $productosFavoritos = [];
+        }
+
+        return view('pages.ver-cliente', compact('cliente', 'productos', 'productosFavoritos'));
     }
 
     // Buscar favoritos
@@ -56,8 +63,50 @@ class ClienteControllerView extends Controller
         $productGuid = $request->input('productoGuid');
         $userId = $request->input('userId');
 
-        Log::info($productGuid . $userId);
+        $cliente = Cliente::where('usuario_id', $userId)->first();
+        if (!$cliente) {
+            Log::info("Cliente con Guid {$cliente->guid} no encontrado");
+            return response()->json(['message' => 'Cliente no encontrado'], 404);
+        }
 
+        $producto = Producto::where('guid', $productGuid)->first();
+        if (!$producto) {
+            Log::info("Producto con Guid {$productGuid} no encontrado");
+            return response()->json(['message' => 'Producto no encontrado'], 404);
+        }
+
+        // Verificar si el producto ya está en favoritos
+        if ($cliente->favoritos()->where('producto_id', $producto->id)->exists()) {
+            Log::info("Producto con Id {$producto->id} ya añadido en favoritos");
+            return response()->json(['status' => 200, 'message' => 'Producto ya añadido en favoritos']);
+        }
+
+        // Agregar el producto a los favoritos usando el 'id' de ambos, cliente y producto
+        $cliente->favoritos()->attach($producto->id);
+
+        Log::info("Producto con Id {$producto->id} agregado a favoritos");
+
+        return response()->json(['status' => 200, 'message' => 'Producto agregado a favoritos']);
+    }
+
+
+    // Quitar un producto de favoritos
+    public function eliminarFavorito(Request $request)
+    {
+        Log::info("Intentando eliminar un producto de favoritos para el cliente logeado");
+
+        $validator = Validator::make($request->all(), [
+            'productoGuid' => 'required',
+            'userId' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            Log::warning('No se han proporcionado los campos necesarios');
+            return response()->json($validator->errors(), 400);
+        }
+
+        $productGuid = $request->input('productoGuid');
+        $userId = $request->input('userId');
 
         $cliente = Cliente::where('usuario_id', $userId)->first();
         if (!$cliente) {
@@ -71,37 +120,11 @@ class ClienteControllerView extends Controller
             return response()->json(['message' => 'Producto no encontrado'], 404);
         }
 
-        // Agregar el producto a los favoritos usando el 'id' de ambos, cliente y producto
-        $cliente->favoritos()->attach($producto->id);
-
-        Log::info("Producto con Id {$producto->id} agregado a favoritos");
-
-        return response()->json(['status' => 200, 'message' => 'Producto agregado a favoritos']);
-    }
-
-
-    // Quitar un producto de favoritos
-    public function eliminarFavorito(Request $request, $guid)
-    {
-        Log::info("Intentando eliminar un producto de favoritos para el cliente con Guid: {$guid}");
-
-        // Buscar el cliente por 'guid'
-        $cliente = Cliente::where('guid', $guid)->first();
-        if (!$cliente) {
-            return response()->json(['message' => 'Cliente no encontrado'], 404);
-        }
-
-        // Buscar el producto por 'id' (no 'guid')
-        $producto = Producto::find($request->producto_id); // Cambiar 'producto_guid' por 'producto_id'
-        if (!$producto) {
-            return response()->json(['message' => 'Producto no encontrado'], 404);
-        }
-
         // Eliminar el producto de los favoritos
-        $cliente->favoritos()->detach($producto->id); // Usamos 'producto_id' aquí
+        $cliente->favoritos()->detach($producto->id);
 
-        Log::info("Producto con Id {$producto->id} eliminado de favoritos para el cliente con guid: {$guid}");
+        Log::info("Producto con Id {$producto->id} eliminado de favoritos");
 
-        return response()->json(['message' => 'Producto eliminado de favoritos']);
+        return response()->json(['status' => 200, 'message' => 'Producto eliminado de favoritos']);
     }
 }
