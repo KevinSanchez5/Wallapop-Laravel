7ac1718e45f553eea735e3e678bd7db27f1e58a0
KevinSanchez5 <@KevinSanchez5>
2025-03-05 19:20:34 +0100

    Automatizacion de estados de ventas

---
+++ waravel/app/Console/Commands/ActualizarEstadoVentas.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Venta;
+use Illuminate\Support\Facades\Log;
+
+class ActualizarEstadoVentas extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:actualizar-estado-ventas';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Actualiza el estado de las ventas cada 24 horas simulando el proceso de entrega de una venta';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Log::info('Iniciando actualizacion de ventas');
+
+        Venta::whereNotIn('estado', ['Entregado', 'Cancelado'])
+            ->update([
+                'estado' =>  \DB::raw("
+                    CASE
+                        WHEN estado = 'Pendiente' THEN 'Procesado'
+                        WHEN estado = 'Procesado' THEN 'Enviado'
+                        WHEN estado = 'Enviado' THEN 'Entregado'
+                        ELSE estado
+                    END
+                "),
+            ]);
+        Log::info('Finalizando actualizacion de ventas');
+        $this->info('Estado de ventas actualizado');
+    }
+}
