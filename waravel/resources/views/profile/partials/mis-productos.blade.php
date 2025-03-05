eb9a14ab8836f1d43f87aaa3b657370e2e81a337
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-03-05 20:56:53 +0100

    broducto borrado mensaje email

--- waravel/resources/views/profile/partials/mis-productos.blade.php
+++ waravel/resources/views/profile/partials/mis-productos.blade.php
@@ -64,22 +64,29 @@
                                 </span>
 
                             <div class="flex items-center gap-2">
-                                <a href="{{ route('producto.edit', $producto->guid) }}" class="p-2 bg-[#BFF205] text-black rounded-md hover:bg-[#A0D500] transition duration-300">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" class="w-5 h-5">
-                                        <path d="M12 20h9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                        <path d="M16.5 3.5a2.121 2.121 0 1 1 3 3L7 19l-4 1 1-4L16.5 3.5z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-                                </a>
-
-                                <form action="{{ route('producto.changestatus', $producto->guid) }}" method="POST" id="deactivateForm">
-                                    @csrf
-                                    @method('POST')
-                                    <button type="button" onclick="showToast('toast-confirm-deactivate')" class="p-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 transition duration-300">
+                                //si el estado del producto es baneado no sale lo siguiente
+                                @if($producto->estado == 'Baneado')
+                                    <span class="inline-flex items-center justify-center px-4 py-2 rounded-full text-white bg-gray-500 w-32 text-center mr-25">
+                                        Baneado
+                                    </span>
+                                @else
+                                    <a href="{{ route('producto.edit', $producto->guid) }}" class="p-2 bg-[#BFF205] text-black rounded-md hover:bg-[#A0D500] transition duration-300">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" class="w-5 h-5">
-                                            <path d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M12 20h9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M16.5 3.5a2.121 2.121 0 1 1 3 3L7 19l-4 1 1-4L16.5 3.5z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                         </svg>
-                                    </button>
-                                </form>
+                                    </a>
+
+                                    <form action="{{ route('producto.changestatus', $producto->guid) }}" method="POST" id="deactivateForm">
+                                        @csrf
+                                        @method('POST')
+                                        <button type="button" onclick="showToast('toast-confirm-deactivate')" class="p-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 transition duration-300">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" class="w-5 h-5">
+                                                <path d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </button>
+                                    </form>
+                                @endif
 
                                 <form action="{{ route('producto.destroy', $producto->guid) }}" method="POST" id="deleteForm">
                                     @csrf
