8e17d9917ac7e474d255aa2b0e418f66e6777da7
KevinSanchez5 <@KevinSanchez5>
2025-03-09 01:48:49 +0100

    Algunos tests de venta

--- waravel/tests/Feature/VentasControllerTest.php
+++ waravel/tests/Feature/VentasControllerTest.php
@@ -6,12 +6,14 @@ use App\Models\Cliente;
 use App\Models\Producto;
 use App\Models\User;
 use App\Models\Venta;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redis;
-use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class VentasControllerTest extends TestCase
 {
+    use RefreshDatabase;
     private User $usuario1;
     private User $usuario2;
     private Cliente $vendedor;
@@ -21,7 +23,7 @@ class VentasControllerTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        Redis::flushall();
+        Redis::isFake();
 
         $this->usuario1 = User::factory()->create(['name' => 'Juan']);
         $this->usuario2 = User::factory()->create(['name' => 'Pedro']);
@@ -43,62 +45,40 @@ class VentasControllerTest extends TestCase
             'stock' => 100,
             'estado' => 'Disponible',
         ]);
-        parent::setUp();
-        Redis::flushAll();
     }
 
     protected function tearDown(): void
     {
-        Redis::flushAll();
+        Redis::isFake();
         parent::tearDown();
     }
 
     public function testIndex()
     {
-        $lineaVentas = [
-            [
-                'vendedor' => $this->vendedor->toArray(),
-                'cantidad' => 5,
-                'producto' => $this->producto->toArray(),
-                'precioTotal' => $this->producto->precio * 5,
-                'estado' => 'Disponible',
+        $venta = Venta::factory()->count(10)->create();
+
+        $response = $this->get('/api/ventas');
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'ventas' => [
+                 '*' => [
+                        'id', 'guid', 'estado', 'comprador', 'lineaVentas', 'precioTotal', 'paymentIntentId', 'created_at', 'updated_at'
+                    ]
             ],
-        ];
-
-        $precioTotalVenta = array_sum(array_column($lineaVentas, 'precioTotal'));
-
-        $venta = Venta::factory()->create([
-            'guid' => 'venta-guid',
-            'comprador' => $this->comprador->toArray(),
-            'lineaVentas' => json_encode($lineaVentas),
-            'precioTotal' => $precioTotalVenta,
-            'estado' => 'Enviado',
+            'paginacion' => [
+                'pagina_actual', 'elementos_por_pagina', 'ultima_pagina', 'elementos_totales'
+            ]
         ]);
 
-        $response = $this->get('/api/ventas?per_page=15');
+        $this->assertCount(5, $response->json('ventas'));
 
-        $response->assertOk();
-
-        $response->assertJsonFragment([
-            'guid' => $venta->guid,
-            'precioTotal' => $venta->precioTotal,
-        ]);
     }
 
     public function testShowVentaFromDatabaseAndStoreInRedis()
     {
-        $venta = Venta::factory()->create([
-            'guid' => 'venta-guid',
-            'comprador' => $this->comprador->toArray(),
-            'lineaVentas' => json_encode([
-                'vendedor' => $this->vendedor->toArray(),
-                'cantidad' => 1,
-                'producto' => $this->producto->toArray(),
-                'precioTotal' => $this->producto->precio,
-            ]),
-            'precioTotal' => $this->producto->precio,
-            'estado' => 'Enviado',
-        ]);
+        $venta = Venta::factory()->create();
 
         Redis::shouldReceive('get')
             ->once()
@@ -115,19 +95,19 @@ class VentasControllerTest extends TestCase
             ->assertJson([
                 'id' => $venta->id,
                 'guid' => $venta->guid,
-                'comprador' => $venta->comprador,
-                'lineaVentas' => $venta->lineaVentas,
+                'comprador' => json_decode(json_encode($venta->comprador), true),
+                'lineaVentas' => json_decode(json_encode($venta->lineaVentas), true),
                 'precioTotal' => round($venta->precioTotal, 2),
                 'estado' => $venta->estado,
+
             ]);
     }
-
 
     public function testShowVentaFromRedis()
     {
         $ventaData = [
             'id' => 1,
-            'guid' => 'venta-test-guid',
+            'guid' => 'venta-test-guid123',
             'comprador' => ['nombre' => 'Juan Pérez', 'email' => 'juan@example.com'],
             'lineaVentas' => [['producto' => 'Laptop', 'cantidad' => 1, 'precio' => 1000.00]],
             'precioTotal' => 1000.00,
@@ -147,8 +127,6 @@ class VentasControllerTest extends TestCase
             ->assertJson($ventaData);
     }
 
-
-
     public function testShowNotFound()
     {
         $response = $this->get("/api/ventas/guid-invalido");
@@ -163,12 +141,21 @@ class VentasControllerTest extends TestCase
     public function testStoreSuccess()
     {
         $data = [
-            'guid' => Str::uuid()->toString(),
             'comprador' => ['nombre' => 'Juan Pérez', 'email' => 'juan@example.com'],
-            'lineaVentas' => [['producto' => 'Laptop', 'cantidad' => 1, 'precio' => 1000.00]],
+            'lineaVentas' => [
+                'vendedor'=>[
+                    'nombre' => 'vendedor',
+                    'apellido' => 'apellido',
+                ] ,
+                'producto' => [
+                    'nombre'=>'Laptop',
+                    'cantidad' => 1,
+                    'precio' => 1000.00
+                ],
+            ],
             'precioTotal' => 1000.00,
             'estado' => 'Entregado',
-        ];
+         ];
 
         $response = $this->post('/api/ventas', $data);
 
@@ -178,15 +165,13 @@ class VentasControllerTest extends TestCase
         ]);
     }
 
-
-
     public function testStoreValidationError()
     {
         $data = [
             'comprador' => [],
             'lineaVentas' => [],
             'precioTotal' => -10,
-            'estado'=> 'no valido'
+            'estado'=> ''
         ];
 
         $response = $this->post('/api/ventas', $data);
@@ -197,25 +182,150 @@ class VentasControllerTest extends TestCase
 
     public function testDestroySuccess()
     {
-        $venta = Venta::factory()->create([]);
+        $venta = Venta::factory()->create();
 
-        $this->assertDatabaseHas('ventas', ['id' => $venta->id]);
+        $this->assertDatabaseHas('ventas', ['guid' => $venta->guid]);
 
-        $response = $this->delete("/api/ventas/{$venta->id}");
+        $response = $this->delete("/api/ventas/{$venta->guid}");
 
         $response->assertStatus(200);
         $response->assertJson(['message' => 'Venta eliminada correctamente']);
 
-        $this->assertDatabaseMissing('ventas', ['id' => $venta->id]);
+        $this->assertDatabaseMissing('ventas', ['guid' => $venta->guid]);
     }
 
     public function testDestroyNotFound()
     {
 
-        $response = $this->delete("/api/ventas/guid-invalido");
+        $response = $this->delete("/api/ventas/guid-inexistente100");
 
         $response->assertStatus(404);
         $response->assertJson(['message' => 'Venta no encontrada']);
     }
+
+    public function testProcesarCompra()
+    {
+
+        $producto = Producto::factory()->create([
+            'estado' => 'Disponible',
+            'stock' => 10,
+            'precio' => 100,
+        ]);
+
+        Auth::loginUsingId(1); // Simulando el login del usuario con ID 1
+
+        session()->put('carrito', (object)[
+            'lineasCarrito' => [
+                (object)[
+                    'producto' => $producto,
+                    'cantidad' => 2
+                ]
+            ]
+        ]);
+
+        $response = $this->postJson(route('pagarcarrito'));
+
+        $response->assertStatus(302); // Redirección al proceso de pago, ajusta según sea necesario
+
+        $response->assertSessionHas('stripe_session_id');
+    }
+
+
+    public function testProcesarCompraCarritoVacio()
+    {
+        Auth::loginUsingId(1);
+
+        session()->put('carrito', (object)[
+            'lineasCarrito' => []
+        ]);
+
+        $response = $this->postJson(route('pagarcarrito'));
+
+        $response->assertStatus(302);
+
+        $response->assertRedirect(route('payment.error'));
+    }
+
+    public function testProcesarCompraConMultiplesProductos()
+    {
+        // Crear cliente y productos de prueba
+        $producto1 = Producto::factory()->create(['estado' => 'Disponible', 'stock' => 10, 'precio' => 50]);
+        $producto2 = Producto::factory()->create(['estado' => 'Disponible', 'stock' => 5, 'precio' => 30]);
+
+        // Simular inicio de sesión
+        Auth::loginUsingId(1);
+
+        // Simular productos en el carrito
+        session()->put('carrito', (object)[
+            'lineasCarrito' => [
+                (object)['producto' => $producto1, 'cantidad' => 2],
+                (object)['producto' => $producto2, 'cantidad' => 1]
+            ]
+        ]);
+
+        // Enviar petición para procesar la compra
+        $response = $this->postJson(route('pagarcarrito'));
+
+        // Verificar redirección y sesión
+        $response->assertStatus(302);
+        $response->assertSessionHas('stripe_session_id');
+    }
+
+    public function testProcesarCompraConStockInsuficiente()
+    {
+        $producto = Producto::factory()->create(['estado' => 'Disponible', 'stock' => 1, 'precio' => 100]);
+
+        Auth::loginUsingId(1);
+
+        session()->put('carrito', (object)[
+            'lineasCarrito' => [
+                (object)['producto' => $producto, 'cantidad' => 5]
+            ]
+        ]);
+
+        $response = $this->postJson(route('pagarcarrito'));
+
+        // Verificar que no se puede procesar la compra
+        $response->assertStatus(302);
+        $response->assertRedirect(route('payment.error'));
+    }
+
+    public function testProcesarCompraSinAutenticacion()
+    {
+        $producto = Producto::factory()->create(['estado' => 'Disponible', 'stock' => 5, 'precio' => 50]);
+
+        session()->put('carrito', (object)[
+            'lineasCarrito' => [
+                (object)['producto' => $producto, 'cantidad' => 2]
+            ]
+        ]);
+
+        $response = $this->postJson(route('pagarcarrito'));
+
+        // Verificar que redirige al login
+        $response->assertStatus(302);
+        $response->assertRedirect(route('pago/error'));
+    }
+
+    public function testProcesarCompraConProductoAgotado()
+    {
+        $producto = Producto::factory()->create(['estado' => 'Desactivado', 'stock' => 0, 'precio' => 75]);
+
+        Auth::loginUsingId(1);
+
+        session()->put('carrito', (object)[
+            'lineasCarrito' => [
+                (object)['producto' => $producto, 'cantidad' => 1]
+            ]
+        ]);
+
+        $response = $this->postJson(route('pagarcarrito'));
+
+        // Verificar que redirige a error
+        $response->assertStatus(302);
+        $response->assertRedirect(route('payment.error'));
+    }
+
+
 
 }
