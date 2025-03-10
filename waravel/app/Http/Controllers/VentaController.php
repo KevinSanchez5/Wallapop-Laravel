a90c26333069526fc6b3fd8d0f9b12ea35495b91
TomasVaquerin <vaquerinjimenez@gmail.com>
2025-03-10 15:41:27 +0100

    VentaController documentacion

--- waravel/app/Http/Controllers/VentaController.php
+++ waravel/app/Http/Controllers/VentaController.php
@@ -597,6 +597,15 @@ class VentaController extends Controller
         return $pdf->download('venta.pdf');
     }
 
+    /**
+     * Envía un correo electrónico con la factura de una venta.
+     *
+     * Este método genera un archivo PDF con los detalles de la venta y lo envía al correo electrónico del comprador.
+     *
+     * @param string $guid El GUID de la venta.
+     * @return \Illuminate\Http\JsonResponse Respuesta JSON indicando el éxito o fallo del envío del correo.
+     */
+
     public function enviarCorreoFactura($guid)
     {
         Log::info('Generando PDF de la venta');
