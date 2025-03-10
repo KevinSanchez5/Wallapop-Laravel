1e94c0749f14032d7f601b5f2dd90498f24a5ea8
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-03-10 15:49:52 +0100

    test

--- waravel/tests/Feature/EmailSenderTest.php
+++ waravel/tests/Feature/EmailSenderTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Mail\EmailSender;
 use InvalidArgumentException;
@@ -38,6 +36,24 @@ class EmailSenderTest extends TestCase
         $this->assertEquals('Producto Comprado en Waravel', $envelope->subject);
     }
 
+    public function testEnvelopeEliminarPerfil()
+    {
+        $email = new EmailSender('usuario', null, null, 'eliminarPerfil');
+        $envelope = $email->envelope();
+
+        $this->assertInstanceOf(Envelope::class, $envelope);
+        $this->assertEquals('Eliminación de perfil - Waravel', $envelope->subject);
+    }
+
+    public function testEnvelopeProductoBorrado()
+    {
+        $email = new EmailSender('usuario', null, 'producto', 'productoBorrado');
+        $envelope = $email->envelope();
+
+        $this->assertInstanceOf(Envelope::class, $envelope);
+        $this->assertEquals('Producto Borrado en Waravel', $envelope->subject);
+    }
+
     public function testContentRecuperarContrasenya()
     {
         $email = new EmailSender('usuario', 'codigo', null, 'recuperarContrasenya');
@@ -56,6 +72,27 @@ class EmailSenderTest extends TestCase
 
         $this->assertInstanceOf(Content::class, $content);
         $this->assertEquals('emails.productoComprado', $content->view);
+        $this->assertArrayHasKey('usuario', $content->with);
+        $this->assertArrayHasKey('producto', $content->with);
+    }
+
+    public function testContentEliminarPerfil()
+    {
+        $email = new EmailSender('usuario', null, null, 'eliminarPerfil');
+        $content = $email->content();
+
+        $this->assertInstanceOf(Content::class, $content);
+        $this->assertEquals('emails.eliminarPerfil', $content->view);
+        $this->assertArrayHasKey('usuario', $content->with);
+    }
+
+    public function testContentProductoBorrado()
+    {
+        $email = new EmailSender('usuario', null, 'producto', 'productoBorrado');
+        $content = $email->content();
+
+        $this->assertInstanceOf(Content::class, $content);
+        $this->assertEquals('emails.productoBorrado', $content->view);
         $this->assertArrayHasKey('usuario', $content->with);
         $this->assertArrayHasKey('producto', $content->with);
     }
